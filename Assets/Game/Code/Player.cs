5d7e57d47424e6c4e99537862a24968fdac3014b
Chotika Maneechot <chotika.man@sbac.ac.th>
2024-04-12 00:04:45 +0700

    Test10

--- Assets/Game/Code/Player.cs
+++ Assets/Game/Code/Player.cs
@@ -6,11 +6,11 @@ using UnityEngine.Tilemaps;
 public class Player : MonoBehaviour
 {
     public GameObject Player1;
-    public GameObject Player2;
-    public GameObject Player3;
-    public GameObject Player4;
+    // public GameObject Player2;
+    // public GameObject Player3;
+    // public GameObject Player4;
 
-    public Tilemap tilemap; 
+    public Tilemap tilemap;
 
     void Start()
     {
@@ -19,19 +19,15 @@ public class Player : MonoBehaviour
 
     void PlayerSpawn()
     {
-        Vector3Int P1_Position = new Vector3Int(0, 12, 0);
-        Vector3Int P2_Position = new Vector3Int(0, 0, 0);
-        Vector3Int P3_Position = new Vector3Int(12, 0, 0);
-        Vector3Int P4_Position = new Vector3Int(12, 12, 0);
-        
-        Vector3 P1_Spawn = tilemap.GetCellCenterWorld(P1_Position);
-        Vector3 P2_Spawn = tilemap.GetCellCenterWorld(P2_Position);
-        Vector3 P3_Spawn = tilemap.GetCellCenterWorld(P3_Position);
-        Vector3 P4_Spawn = tilemap.GetCellCenterWorld(P4_Position);
+        Vector3 P1_Position = tilemap.GetCellCenterWorld(new Vector3Int(0, 12, 0));
+        // Vector3 P2_Position = tilemap.GetCellCenterWorld(new Vector3Int(0, 0, 0));
+        // Vector3 P3_Position = tilemap.GetCellCenterWorld(new Vector3Int(12, 0, 0));
+        // Vector3 P4_Position = tilemap.GetCellCenterWorld(new Vector3Int(12, 12, 0));
 
-        Instantiate(Player1, P1_Spawn, Quaternion.identity);
-        Instantiate(Player2, P2_Spawn, Quaternion.identity);
-        Instantiate(Player3, P3_Spawn, Quaternion.identity);
-        Instantiate(Player4, P4_Spawn, Quaternion.identity);
+        Instantiate(Player1, P1_Position, Quaternion.identity);
+        // Instantiate(Player2, P2_Position, Quaternion.identity);
+        // Instantiate(Player3, P3_Position, Quaternion.identity);
+        // Instantiate(Player4, P4_Position, Quaternion.identity);
     }
+    
 }
